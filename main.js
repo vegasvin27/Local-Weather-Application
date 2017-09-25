76cb032c68678f53f8fc3ab274deb021fafe4692
Vincent Tripi <vinnyt95@yahoo.com>
2017-09-25 19:33:49 -0400

    Added a starter HTML page and JavaScript file
    By grabbing current local weather data.

---
+++ main.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+
+    $.get("https://api.openweathermap.org/data/2.5/weather?lat=33.872979&lon=-78.5559668&appid=b52681a81662a7533c219afee17a3dee", function(data) {
+        console.log(data);
+
+        const cityName = data.name;
+        console.log(cityName);
+        const currentTemp = data.main.temp;
+        const windSpeed = data.wind.speed;
+        console.log(windSpeed);
+        //console.log(currentTemp);
+        $('#name').append(cityName);
+        $('#windSpeed').append(windSpeed);        
+
+        //Function to convert Kelivin to Fahrenheit
+        function convertToK (currentTemp) {
+            var fahrenheit = parseFloat((currentTemp * 9/5) - 459.67).toFixed(1);
+            console.log(fahrenheit);
+            $('#temp').append(fahrenheit + 'F');
+        }
+
+        convertToK(currentTemp);
+
+        //Function to convert Fahrenheit to Celsius
+        function convertToC () {
+
+        }
+
+        //Grabbing Geo-location coordinates
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+              $("#data").html("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
+                console.log(position.coords.latitude,  position.coords.longitude);
+            });
+          }
+        });
+    });
+
+
+//http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=580995f792e0d4a822eb2ed4d6a642d5
+
+//b52681a81662a7533c219afee17a3dee
+
+
+
+
+
+
