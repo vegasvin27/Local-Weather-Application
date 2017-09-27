f2dcde146df798d9104d7312ae61d6c2aca2c856
Tabias Pittman <tabiaspittman94@gmail.com>
2017-09-27 03:50:25 +0000

    updated location

--- main.js
+++ main.js
@@ -1,52 +1,42 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    function success (position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-    
+     //Grabbing Geo-location coordinates
+     var lat = 0;
+     var long = 0;
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                $("#data").html("Latitude: " + latitude  + "<br>Longitude: " + longitude);
-                console.log(latitude, longitude);
+            navigator.geolocation.getCurrentPosition(function(position) {
+                lat = position.coords.latitude;
+                long = position.coords.longitude;
+              $("#data").html("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
+                console.log(position.coords.latitude,  position.coords.longitude);
+                $.get("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid=b52681a81662a7533c219afee17a3dee", function(data) {
+                console.log(data);
+
+                const cityName = data.name;
+                console.log(cityName);
+                const currentTemp = data.main.temp;
+                const windSpeed = data.wind.speed;
+                console.log(windSpeed);
+                //console.log(currentTemp);
+                $('#name').append(cityName);
+                $('#windSpeed').append(windSpeed);        
+
+                //Function to convert Kelivin to Fahrenheit
+                function convertToK (currentTemp) {
+                    var fahrenheit = parseFloat((currentTemp * 9/5) - 459.67).toFixed(1);
+                    console.log(fahrenheit);
+                    $('#temp').append(fahrenheit + 'F');
+                }
+
+                convertToK(currentTemp);
+
+                //Function to convert Fahrenheit to Celsius
+                function convertToC () {
+
+                }
             });
-        }
+        });
     }
-
-
-    $.get("https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=b52681a81662a7533c219afee17a3dee", function (data) {
-
-        console.log(data);
-
-        const cityName = data.name;
-        console.log(cityName);
-        const currentTemp = data.main.temp;
-        const windSpeed = data.wind.speed;
-        console.log(windSpeed);
-        //console.log(currentTemp);
-        $('#name').append(cityName);
-        $('#windSpeed').append(windSpeed);
-
-        //Function to convert Kelivin to Fahrenheit
-        function convertToK(currentTemp) {
-            var fahrenheit = parseFloat((currentTemp * 9 / 5) - 459.67).toFixed(1);
-            console.log(fahrenheit);
-            $('#temp').append(fahrenheit + 'F');
-        }
-
-        convertToK(currentTemp);
-
-        //Function to convert Fahrenheit to Celsius
-        function convertToC() {
-
-        }
-
-        
-
-
-
-        //Grabbing Geo-location coordinates
-
-    });
 });
 
 
