5ab88517122c1666a47708a48dbfaee36a773c9b
Vincent Tripi <vinnyt95@yahoo.com>
2017-09-27 00:09:59 -0400

    Fixed location issue

--- main.js
+++ main.js
@@ -1,41 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    
+    
+    //Grabbing Geo-location coordinates
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var lat = position.coords.latitude;
+            var lon = position.coords.longitude;
+            $("#data").html("Latitude: " + lat + "<br>Longitude: " + lon);
+            console.log(lat, lon);
+            $.get("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=b52681a81662a7533c219afee17a3dee", function (data) {
 
+                console.log(data);
 
-    $.get("https://api.openweathermap.org/data/2.5/weather?lat=33.872979&lon=-78.5559668&appid=b52681a81662a7533c219afee17a3dee", function(data) {
-        console.log(data);
+                const cityName = data.name;
+                console.log(cityName);
+                const currentTemp = data.main.temp;
+                const windSpeed = data.wind.speed;
+                console.log(windSpeed);
+                //console.log(currentTemp);
+                $('#name').append(cityName);
+                $('#windSpeed').append(windSpeed);
 
-        const cityName = data.name;
-        console.log(cityName);
-        const currentTemp = data.main.temp;
-        const windSpeed = data.wind.speed;
-        console.log(windSpeed);
-        //console.log(currentTemp);
-        $('#name').append(cityName);
-        $('#windSpeed').append(windSpeed);        
+                //Function to convert Kelivin to Fahrenheit
+                function convertToK(currentTemp) {
+                    var fahrenheit = parseFloat((currentTemp * 9 / 5) - 459.67).toFixed(1);
+                    console.log(fahrenheit);
+                    $('#temp').append(fahrenheit + 'F');
+                }
 
-        //Function to convert Kelivin to Fahrenheit
-        function convertToK (currentTemp) {
-            var fahrenheit = parseFloat((currentTemp * 9/5) - 459.67).toFixed(1);
-            console.log(fahrenheit);
-            $('#temp').append(fahrenheit + 'F');
-        }
+                convertToK(currentTemp);
 
-        convertToK(currentTemp);
+                //Function to convert Fahrenheit to Celsius
+                function convertToC() {
 
-        //Function to convert Fahrenheit to Celsius
-        function convertToC () {
-
-        }
-
-        //Grabbing Geo-location coordinates
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-              $("#data").html("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
-                console.log(position.coords.latitude,  position.coords.longitude);
+                }
             });
-          }
         });
-    });
+
+
+    }
+
+});
 
 
 //http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=580995f792e0d4a822eb2ed4d6a642d5
