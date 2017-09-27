2ea47432e22ee0716c12c2772b53b7d7c1eb2d5d
Vincent Tripi <vinnyt95@yahoo.com>
2017-09-27 18:57:35 -0400

    Added temp conv func, toggle button for temp, and icon for weather description

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    
-    
+
+
     //Grabbing Geo-location coordinates
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -19,27 +19,63 @@ $(document).ready(function () {
                 console.log(windSpeed);
                 //console.log(currentTemp);
                 $('#name').append(cityName);
-                $('#windSpeed').append(windSpeed);
+                const cloudiness = data.weather[0].description;
+                const icon = data.weather[0].icon;
+                const iconLink = ('http://openweathermap.org/img/w/'+icon+'.png');
+                console.log(iconLink);
+                console.log(cloudiness);
+                console.log(icon);
+                $("#clouds").append(cloudiness);
+                $("#icon").html("<img src='" + iconLink + "'>");
+                
 
                 //Function to convert Kelivin to Fahrenheit
+                var fahrenheit = parseFloat((currentTemp * (9 / 5)) - 459.67).toFixed(1);
                 function convertToK(currentTemp) {
-                    var fahrenheit = parseFloat((currentTemp * 9 / 5) - 459.67).toFixed(1);
                     console.log(fahrenheit);
-                    $('#temp').append(fahrenheit + 'F');
+                    $('#fah').append(fahrenheit + 'F');
+                    //$('#temp').append($("button"));
                 }
 
                 convertToK(currentTemp);
 
                 //Function to convert Fahrenheit to Celsius
-                function convertToC() {
-
+                var celsius = parseFloat((fahrenheit - 32) * (5 / 9)).toFixed(1);
+                function convertToC(fahrenheit) {
+                    console.log(celsius);
+                    $('#cel').append(celsius + 'C');
                 }
+                convertToC(fahrenheit);
+
+                /* $('#test').click(function() {
+                    $('#test').fadeOut(function() {
+                        $('#test').text(($('#test').text() == 'F') ? 'C' : 'F').fadeIn();
+                    })
+                }) */
+
+                /* $("#test").click(function () {
+                    $("#test").toggle();
+                }); */
+                //Function to convert wind speed from m/s to mph
+                function convertWindSpeed(windSpeed) {
+                    //Convert m/s to mph 
+                    //1 meter per second = 2.237 mph
+                    //const speed = 2.237;
+                    var mph = parseFloat((windSpeed * 2.237).toFixed(1));
+                    console.log(mph);
+                    $('#windSpeed').append(mph + ' mph');
+                }
+                convertWindSpeed(windSpeed);
+
+
+                $("#button").click(function () {
+                    /*  $("#button1").remove();
+                     $("#button2").toggle('button2'); */
+                    $("p").toggle('cel');
+                });
             });
         });
-
-
     }
-
 });
 
 
@@ -47,8 +83,43 @@ $(document).ready(function () {
 
 //b52681a81662a7533c219afee17a3dee
 
+/*if currentTemp === fahrenheit
+    $('temp').addeventlistener('click')
+*/
 
+/*
 
+$("button").click(function() {
 
+    $("#temp").click(function() {
+        $("#temp").fadeOut(function() {
+            $("#temp").text(($("#temp").text() == fahrenheit) ? celcius: fahrenheit).fadeIn();
+        })
+    })
+});
 
+*/
 
+/*
+
+$([fahrenheit]").click(function() {
+
+    $("fahrenheit variable").toggle();
+    //celcius display needs to be set to 'none'
+});
+*/
+
+/*
+$("#test").click(function () {
+    $("#test").toggle();
+});
+*/
+
+/* <button>Toggle</button>
+    <p>Hello</p>
+    <p style="display: none">Good Bye</p>
+
+    <script>
+        $( "button" ).click(function() {
+            $("p").toggle();
+        }); */
