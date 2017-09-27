d9cefcbcfb5a8f738388f6778c7d5062807bcb82
Vincent Tripi <vinnyt95@yahoo.com>
2017-09-26 23:24:17 -0400

    testing

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    function success (position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+    
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                $("#data").html("Latitude: " + latitude  + "<br>Longitude: " + longitude);
+                console.log(latitude, longitude);
+            });
+        }
+    }
 
 
-    $.get("https://api.openweathermap.org/data/2.5/weather?lat=33.872979&lon=-78.5559668&appid=b52681a81662a7533c219afee17a3dee", function(data) {
+    $.get("https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=b52681a81662a7533c219afee17a3dee", function (data) {
+
         console.log(data);
 
         const cityName = data.name;
@@ -11,11 +24,11 @@ $(document).ready(function() {
         console.log(windSpeed);
         //console.log(currentTemp);
         $('#name').append(cityName);
-        $('#windSpeed').append(windSpeed);        
+        $('#windSpeed').append(windSpeed);
 
         //Function to convert Kelivin to Fahrenheit
-        function convertToK (currentTemp) {
-            var fahrenheit = parseFloat((currentTemp * 9/5) - 459.67).toFixed(1);
+        function convertToK(currentTemp) {
+            var fahrenheit = parseFloat((currentTemp * 9 / 5) - 459.67).toFixed(1);
             console.log(fahrenheit);
             $('#temp').append(fahrenheit + 'F');
         }
@@ -23,19 +36,18 @@ $(document).ready(function() {
         convertToK(currentTemp);
 
         //Function to convert Fahrenheit to Celsius
-        function convertToC () {
+        function convertToC() {
 
         }
 
+        
+
+
+
         //Grabbing Geo-location coordinates
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-              $("#data").html("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
-                console.log(position.coords.latitude,  position.coords.longitude);
-            });
-          }
-        });
+
     });
+});
 
 
 //http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=580995f792e0d4a822eb2ed4d6a642d5
