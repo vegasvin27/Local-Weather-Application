c27698dbc9d02e25c2870a2cad8eca2b28032c7e
Vincent Tripi <vinnyt95@yahoo.com>
2017-09-29 15:36:33 -0400

    Removed console.log statements and added more comments

--- main.js
+++ main.js
@@ -1,87 +1,63 @@
 $(document).ready(function () {
 
-
     //Grabbing Geo-location coordinates
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var lat = position.coords.latitude;
             var lon = position.coords.longitude;
             $("#data").html("Latitude: " + lat + "<br>Longitude: " + lon);
-            console.log(lat, lon);
             $.get("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=b52681a81662a7533c219afee17a3dee", function (data) {
 
-                console.log(data);
+                //If need to see object data, just console.log(data)
 
                 const cityName = data.name;
-                console.log(cityName);
                 const currentTemp = data.main.temp;
                 const windSpeed = data.wind.speed;
-                console.log(windSpeed);
-                //console.log(currentTemp);
-                $('#name').append(cityName);
                 const cloudiness = data.weather[0].main;
                 const icon = data.weather[0].icon;
-                const iconLink = ('http://openweathermap.org/img/w/'+icon+'.png');
-                console.log(iconLink);
-                console.log(cloudiness);
-                console.log(icon);
+                const iconLink = ('http://openweathermap.org/img/w/' + icon + '.png');
+                const windDegrees = data.wind.deg;
+
+                //Appending weather data to html elements
+                $('#name').append(cityName);
                 $("#clouds").append(cloudiness);
                 $("#icon").html("<img src='" + iconLink + "'>");
-                const windDegrees = data.wind.deg;
-                console.log(windDegrees);
 
                 //Function to convert Kelivin to Fahrenheit
                 var fahrenheit = parseFloat((currentTemp * (9 / 5)) - 459.67).toFixed(1);
                 function convertToK(currentTemp) {
-                    console.log(fahrenheit);
                     $('#fah').append(fahrenheit + ' &#8457;');
                     //$('#temp').append($("button"));
                 }
-
                 convertToK(currentTemp);
 
                 //Function to convert Fahrenheit to Celsius
                 var celsius = parseFloat((fahrenheit - 32) * (5 / 9)).toFixed(1);
                 function convertToC(fahrenheit) {
-                    console.log(celsius);
                     $('#cel').append(celsius + ' &#8451;');
                 }
                 convertToC(fahrenheit);
 
-                /* $('#test').click(function() {
-                    $('#test').fadeOut(function() {
-                        $('#test').text(($('#test').text() == 'F') ? 'C' : 'F').fadeIn();
-                    })
-                }) */
-
-                /* $("#test").click(function () {
-                    $("#test").toggle();
-                }); */
-                //Function to convert wind speed from m/s to mph
+                //Function to convert wind speed from meters per second to miles per hour
                 function convertWindSpeed(windSpeed) {
-                    //Convert m/s to mph 
-                    //1 meter per second = 2.237 mph
-                    //const speed = 2.237;
+                    //1 meter per second = 2.237 miles per hour
                     var mph = parseFloat((windSpeed * 2.237).toFixed(1));
-                    console.log(mph);
                     $('#windSpeed').append(mph + ' mph ');
                 }
                 convertWindSpeed(windSpeed);
 
+                //Function to convert wind degrees (0-360) to a cardinal direction
                 function degToCompass(windDegrees) {
                     var val = Math.floor((windDegrees / 22.5) + 0.5);
                     var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
                     var direction = (arr[(val % 16)]);
-                    console.log(direction);
-                    console.log(val);
                     $('#windSpeed').append(direction);
                 }
-
                 degToCompass(windDegrees);
 
                 $("button").click(function () {
                     /*  $("#button1").remove();
-                     $("#button2").toggle('button2'); */
+                    $("#button2").toggle('button2'); */
                     $(".tp").toggle('cel');
                 });
             });
@@ -89,19 +65,15 @@ $(document).ready(function () {
     }
 });
 
-
-//http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=580995f792e0d4a822eb2ed4d6a642d5
-
-//b52681a81662a7533c219afee17a3dee
+//Testing section
 
 /*if currentTemp === fahrenheit
-    $('temp').addeventlistener('click')
+$('temp').addeventlistener('click')
 */
-
 /*
 
 $("button").click(function() {
-
+    
     $("#temp").click(function() {
         $("#temp").fadeOut(function() {
             $("#temp").text(($("#temp").text() == fahrenheit) ? celcius: fahrenheit).fadeIn();
@@ -114,7 +86,7 @@ $("button").click(function() {
 /*
 
 $([fahrenheit]").click(function() {
-
+    
     $("fahrenheit variable").toggle();
     //celcius display needs to be set to 'none'
 });
@@ -127,10 +99,21 @@ $("#test").click(function () {
 */
 
 /* <button>Toggle</button>
-    <p>Hello</p>
-    <p style="display: none">Good Bye</p>
+<p>Hello</p>
+<p style="display: none">Good Bye</p>
 
-    <script>
-        $( "button" ).click(function() {
-            $("p").toggle();
-        }); */
+<script>
+$( "button" ).click(function() {
+    $("p").toggle();
+}); */
+
+
+/* $('#test').click(function() {
+    $('#test').fadeOut(function() {
+        $('#test').text(($('#test').text() == 'F') ? 'C' : 'F').fadeIn();
+    })
+}) */
+
+/* $("#test").click(function () {
+    $("#test").toggle();
+}); */
